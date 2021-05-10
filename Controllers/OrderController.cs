e826fbfbff659fc9a96a5ec7f820418d5d773e76
myildizay <myildizay@hotmail.com>
2021-05-10 19:09:04 +0300

    Order Controller has been added

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -12,9 +12,21 @@ namespace Northwind.Controllers
     public class OrderController : ApiController
     {
         NorthwindEntities db = new NorthwindEntities();
-        
+
+
+        public IEnumerable<Order> Get()
+        {
+            return db.Orders.ToList();
+        }
+
+        public Order Get(int id)
+        {
+            return db.Orders.FirstOrDefault(x => x.OrderID == id);
+        }
+
+
         [System.Web.Http.HttpGet]
-        //https://localhost:44325/api/order/OrderParamByEmployeeId/?employeeId=1
+        //https://localhost:44325/api/order/OrderParamByEmployeeId/employeeId=1
         public HttpResponseMessage OrderParamByEmployeeId(int? employeeId= 0)
         {
             db.Configuration.ProxyCreationEnabled = false;
@@ -43,7 +55,7 @@ namespace Northwind.Controllers
 
 
         [System.Web.Http.HttpGet]
-        //https://localhost:44325/api/order/OrderParamByCustomerShipCountryShipCity/?customerId=VINET&shipCountry=France&shipCity=Reims
+        //https://localhost:44325/api/order/OrderParamByCustomerShipCountryShipCity?customerId=VINET&shipCountry=France&shipCity=Reims
         public HttpResponseMessage OrderParamByCustomerShipCountryShipCity(string  customerId ="" , string shipCountry = "", string shipCity ="")
         {
             db.Configuration.ProxyCreationEnabled = false;
@@ -63,5 +75,23 @@ namespace Northwind.Controllers
 
             return Request.CreateResponse(HttpStatusCode.OK, query.ToList());
         }
+
+        [System.Web.Http.HttpGet]
+        //https://localhost:44325/api/order/OrderParamByShipName?shipName=TOMS
+        public HttpResponseMessage OrderParamByShipName(string shipName="")
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            IQueryable<Order> query = db.Orders;
+            if (shipName != "")
+            {
+                query = query.Where(e => e.ShipName.Contains(shipName));
+            }
+            
+
+            return Request.CreateResponse(HttpStatusCode.OK, query.ToList());
+        }
+
+
+
     }
 }
